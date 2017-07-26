e773c605c30b8513f9b1839db88e98a390183505
gandalf the white <gandalf_the_white@orange.fr>
2017-07-26 16:31:08 +0200

    Create session_tmux.sh

---
+++ session_tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh 
+
+eval `ssh-agent`
+ssh-add ~/.ssh/id_rsa
+
+tmux new-session  -d -s 'the_hack' -n 'the hack' 'ssh the_hack'
+tmux new-window   -n 'the maas' 'ssh the_maas'
+tmux new-window   -n 'the openstack' 'ssh the_openstack'
+#tmux split-window -v 'python'
+#tmux split-window -h
+#tmux new-window 'top'
+tmux -2 attach-session -d 

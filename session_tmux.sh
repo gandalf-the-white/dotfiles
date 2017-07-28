d7d7b6ebdb2c4008606bc2ba8003edcf5edb5aab
gandalf the white <gandalf_the_white@orange.fr>
2017-07-28 14:20:37 +0200

    Update session_tmux.sh

--- session_tmux.sh
+++ session_tmux.sh
@@ -5,10 +5,13 @@
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 
-tmux new-session  -d -s 'the_hack' -n 'the hack' 'ssh the_hack'
-tmux new-window   -n 'the maas' 'ssh the_maas'
-tmux new-window   -n 'the openstack' 'ssh the_openstack'
-#tmux split-window -v 'python'
-#tmux split-window -h
-#tmux new-window 'top'
+tmux new-session  -d -s 'Cowboy Bebop' -n 'faye'
+tmux split-window -h
+tmux split-window -v
+tmux new-window -n 'the hack' 'ssh the_hack'
+tmux new-window -n 'the maas' 'ssh the_maas'
+tmux new-window -n 'the openstack' 'ssh the_openstack'
+tmux new-window -n 'the cdpweather' 'ssh the_cdpweather'
+tmux new-window -n 'the database' 'ssh the_database'
+
 tmux -2 attach-session -d 
